c4b129687b7f9859c82ea9c355a4528b6446abe0
sti-mtokamae <mtokamae@sojitz-ti.com>
2026-05-29 19:58:16 +0900

    deleted:    trials/dry-run-count.clj

--- trials/merge-one-class.clj
+++
@@ -1,8 +0,0 @@
-(require 'workbench.core)
-(workbench.core/start!)
-(workbench.core/merge-test-mds
-  :gen-dir "trials/experiments/2026-04-28-tradehub/exports/gen-tests"
-  :class   "DocumentsVerificationDataServiceImpl"
-  :force   true)
-(workbench.core/stop!)
-(System/exit 0)
